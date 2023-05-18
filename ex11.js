cab31f2514c84fe2280cd63db75158d55ebe1566
Andressa-Evelyn <andressaevelyn200214@gmail.com>
2023-05-18 14:33:58 -0300

    horas

---
+++ ex11.js
@@ -0,0 +1,10 @@
+var idade = 17;
+if (idade > 16 && idade < 18) {
+    console.log('Vota e é menor de idade')
+} else 
+    if(idade > 18) {
+        console.log('Vota e é maior de idade')
+    } else if (idade < 16) {
+        console.log('Não vota')
+    
+}
---
+++ ex13.js
@@ -0,0 +1,15 @@
+var agora = new Date();
+var dia = agora.getDay();
+
+switch(dia) {
+case 0:
+    console.log('Domingo')
+    break;
+case 1:
+    console.log('Segunda')
+    break;
+case 4:
+    console.log('Quinta')
+    break;
+} 
+

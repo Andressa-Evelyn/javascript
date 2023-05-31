38c7d65b189c1c391a81ce571d4ebcccc921f06a
Andressa-Evelyn <andressaevelyn200214@gmail.com>
2023-05-31 14:15:40 -0300

    exercicio

--- aula6/exercicios/script.js
+++ aula6/exercicios/script.js
@@ -1,1 +1,17 @@
-alert('Olá')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = document.getElementById('foto')
+    var data =new Date();
+    var hora = data.getHours()
+    
+    msg.innerHTML = `Agora são ${hora} horas`
+    
+    
+    if(hora >= 6 && hora < 12) {
+        foto.src = 'manha.jpg'
+    } else if( hora >= 12 && hora < 18) {
+        foto.src = 'tarde.jpg'
+    } else {
+        foto.src = 'noite.jpg'
+    }
+    };
